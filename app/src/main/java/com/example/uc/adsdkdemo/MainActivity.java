682d000f0fbd1c9f27ada1ba5750de476456383e
xiaojun <xj146543@alibaba-inc.com>
2017-10-13 15:41:17 +0800

    YYYYY

--- app/src/main/java/com/example/uc/adsdkdemo/MainActivity.java
+++ app/src/main/java/com/example/uc/adsdkdemo/MainActivity.java
@@ -92,35 +92,12 @@ public class MainActivity extends Activity implements AdapterView.OnItemSelected
         }
     }
 
-    private void m1() {
+
+    private void v1() {
 
     }
 
-    private void m2() {
-
-    }
-
-    private void m3() {
-
-    }
-
-    private void t1() {
-
-    }
-
-    private void t2() {
-
-    }
-
-    private void t3() {
-
-    }
-
-    private void q() {
-
-    }
-
-    private void q1() {
+    private void v2() {
 
     }
 
