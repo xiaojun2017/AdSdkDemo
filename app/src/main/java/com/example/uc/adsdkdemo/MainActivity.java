61d9c390fbc003c9db498a1bfa79069116eac1b0
xiaojun <xj146543@alibaba-inc.com>
2017-10-13 14:13:29 +0800

    add test7 method

--- app/src/main/java/com/example/uc/adsdkdemo/MainActivity.java
+++ app/src/main/java/com/example/uc/adsdkdemo/MainActivity.java
@@ -128,6 +128,10 @@ public class MainActivity extends Activity implements AdapterView.OnItemSelected
     }
 
     private void test6() {
-        
+
+    }
+
+    private void test7() {
+
     }
 }
